44ce7627d0752050ec684e846e475865b6d59e54
samuelfreire03 <s.freire@uniandes.edu.co>
2023-05-04 16:53:58 -0500

    cambios

--- measurements/logic/logic_measurement.py
+++ measurements/logic/logic_measurement.py
@@ -8,3 +8,14 @@ def create_measurement(form):
     measurement = form.save()
     measurement.save()
     return ()
+
+def create_measurement_object(variable_id, fecha, lugar, tipo,motivo,enfer):
+    measurement = Measurement()
+    measurement.paciente = variable_id
+    measurement.fechaConsulta = fecha
+    measurement.lugarConsulta = lugar
+    measurement.tipoConsulta = tipo
+    measurement.motivoConsulta = motivo
+    measurement.enfermedad = enfer
+    measurement.save()
+    return ()
