5d170cb007966c1bf02532c3f722cbc18d26d3db
samuelfreire03 <s.freire@uniandes.edu.co>
2023-05-10 19:52:43 -0500

    cambios adendas

---
+++ adendas/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .forms import AdendaForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .logic.logic_adenda import create_adenda, get_adendas
+
+def adenda_list(request):
+    adendas = get_adendas()
+    context = {
+        'adenda_list': adendas
+    }
+    return render(request, 'Adenda/adendas.html', context)
+
+def adenda_create(request):
+    if request.method == 'POST':
+        form = AdendaForm(request.POST)
+        if form.is_valid():
+            create_adenda(form)
+            messages.add_message(request, messages.SUCCESS, 'Adenda create successful')
+            return HttpResponseRedirect(reverse('adendaCreate'))
+        else:
+            print(form.errors)
+    else:
+        form = AdendaForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'Adenda/adendaCreate.html', context)
